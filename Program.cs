d351331f9327b1d83dd14389f92fc17ed93f195a
Alaa Shams <alaashams490@gmail.com>
2026-09-05 17:18:30 +0300

    feat: complete async lab dispatcher with Task.WhenAny

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("requsting a report from each lab branch...");
+
+task<String> cairoTask = FetchFromCairoLabAsync();
+task<String> gizaTask = FetchFromGizaLabAsync();
+task <String> alexandriaTask = FetchFromAlexandriaLabAsync();
+task <String> fastestTask = Task.WhenAny(cairoTask, gizaTask, alexandriaTask);
+string fastestReport = await fastestTask;
+Console.WriteLine($"The fastest report is: {fastestReport}");
+
+
+async Task<string>  FetchFromCairoLabAsync()
+await task delay(2000);
+return ("Report from Cairo Central");
+async Task<string> FetchFromGizaLabAsync()
+await task delay(1000);
+return("Report from Giza Lab");
+async Task<string> FetchFromAlexandriaLabAsync()
+await task delay(3000);
+return("Report from Alexandria Lab");
